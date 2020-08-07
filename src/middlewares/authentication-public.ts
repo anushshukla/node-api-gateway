88e2d635f04e9a2904bbc9242d9fdfd5826d235d
Anush Shukla <anush.s@servify.in>
2020-08-07 20:03:27 +0530

    public authentication updated

--- src/middlewares/authentication-public.ts
+++ src/middlewares/authentication-public.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import axios from 'axios';
+import safePromise from "../utils/safe-promise";
 
 const isAuthorisationValid = (clientId: string, host: string, date: string, hmac: string, clientSessionId: string): boolean => {
   // @ToDo check redis of public prime api
@@ -17,18 +19,19 @@ type isAnyValueEmptyType = (...argument: any[]) => boolean;
 const isAnyValueEmpty: isAnyValueEmptyType = (...authorisationKeys) => {
   let isEmpty = false;
   authorisationKeys.map(authorisationKey => {
-      if (!authorisationKey) {
-        isEmpty = true;
-        return;
-      }
+    if (!authorisationKey) {
+      isEmpty = true;
+      return;
+    }
   });
   return isEmpty;
 }
-export default () => (
+type PublicAuthentication = void | Response<any>;
+export default () => async (
   request: Request,
   response: Response,
   next: NextFunction,
-): void | Response<any> => {
+): Promise<PublicAuthentication> => {
   const { headers: {
     client_id: clientId,
     x_host: host,
@@ -36,12 +39,23 @@ export default () => (
     hmac_signature: hmac,
     client_session_id: clientSessionId,
   } } = request;
-  
+
   if (isAnyValueEmpty(clientId, host, date, hmac, clientSessionId)) {
     return response.sendStatus(403)
   }
-  if (isAuthorisationValid(<string> clientId, <string> host, <string> date, <string> hmac, <string> clientSessionId)) {
+  if (isAuthorisationValid(<string>clientId, <string>host, <string>date, <string>hmac, <string>clientSessionId)) {
     return response.sendStatus(401)
+  }
+  const primeUrl = 'http://172.21.3.28:4000/api/v1/authentication/check';
+  const [error, primeResponse] = await safePromise(axios.get(primeUrl));
+  if (error) {
+    return next(error)
+  }
+  if (!primeResponse) {
+    return response.sendStatus(500);
+  }
+  if (primeResponse.status !== 200) {
+    response.send(response);
   }
   // check token table prod_servify DB
   // @ToDo call Core API for authorisation token validation
